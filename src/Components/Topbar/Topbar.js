663650e8c3e3b247afec075aa01997e0bd111666
Saj Polwa <sajpolwa@gmail.com>
2022-04-23 03:13:30 -0400

    Logo created
    logo created, which is a link.
    When logo clicked it brings you back to the home (intro page).
    When logo clicked the transition to the intro page will scroll smooth.
    gave id to all components.
    created global css that has theme colors

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -2,6 +2,15 @@ import './topbar.scss'
 
 export default function Topbar() {
     return (
-        <div className='topbar'></div>
+        <div className='topbar'>
+            <div className='wrapper'>
+                <div className='left'>
+                    <a href='#intro' className='logo'>Saj Polwatta</a>
+                </div>
+                <div className='right'>
+
+                </div>
+            </div>
+        </div>
     )
 }
