697ef2a68a66f0d75f4d333723ef783fba32049c
Saj Polwa <sajpolwa@gmail.com>
2022-04-25 23:29:25 -0400

    imported react icons
    added colormode icon

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,4 +1,5 @@
 import './topbar.scss'
+import {VscColorMode} from 'react-icons/vsc'
 
 export default function Topbar() {
     return (
@@ -6,6 +7,10 @@ export default function Topbar() {
             <div className='wrapper'>
                 <div className='left'>
                     <a href='#intro' className='logo'>Saj Polwatta</a>
+                    <div className='colorMode'>
+                        <VscColorMode/>
+                            <span>DarkMode</span>
+                    </div>
                 </div>
                 <div className='right'>
 
