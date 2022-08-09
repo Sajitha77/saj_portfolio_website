c929f73e213a1f38193dae6d5d30eb2c2d0961cf
Saj Polwa <sajpolwa@gmail.com>
2022-08-08 23:20:52 -0400

    Home page Updated
    Basic Layout of home page created
    Image needs to be replace
    Fonts and themes need to change

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -3,6 +3,22 @@ import './intro.scss'
 export default function Intro() {
     return (
         <div className='intro' id='intro'>
+            <div className='left'>
+                <div className='imageContainer'>
+                    <img src='assets/profile.jpg' alt='profile'></img>
+                </div>
+            </div>
+
+            <div className='right'>
+                <div className='wrapper'>
+                    <h2>Hi There! I am</h2>
+                    <h1>Saj Polwatta!</h1>
+                    <h3>Computer Science Student at Carleton University</h3>
+                </div>
+                <a href='#protfolio'>
+                    
+                </a>
+            </div>
         </div>
     )
 }
