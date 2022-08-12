e7bf1313df20e29cd630de6509b26855cb6c5b8a
Saj Polwa <sajpolwa@gmail.com>
2022-08-12 01:30:21 -0400

    intro page
    changed intro with no image and moved image to portfolio
    
    intent- home page/intro will be very minimal and creative
    portfolio will not become about me page
    works will become portfolio
    testimonial will become skills
    contact page remains the same

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -4,9 +4,6 @@ export default function Intro() {
     return (
         <div className='intro' id='intro'>
             <div className='left'>
-                <div className='imageContainer'>
-                    <img src='assets/profile.jpg' alt='profile'></img>
-                </div>
             </div>
 
             <div className='right'>
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -3,6 +3,11 @@ import './portfolio.scss'
 export default function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
+            <div className='left'>
+                <div className='imageContainer'>
+                    <img src='assets/profile.jpg' alt='profile'></img>
+                </div>
+            </div>
         </div>
     )
 }
