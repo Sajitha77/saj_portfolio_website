2919630877fce4a9f055a4273a737c0419f80b13
Saj Polwa <sajpolwa@gmail.com>
2022-08-13 02:12:53 -0400

    small addition
    small text addition

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -4,10 +4,14 @@ export default function Intro() {
     return (
         <div className='intro' id='intro'>
             <div className='left'>
+                <div className='leftWrapper'>
+                    <h1>Welcome!</h1>
+                    <h2>Explore and get to know me!</h2>
+                </div>
             </div>
 
             <div className='right'>
-                <div className='wrapper'>
+                <div className='rightWrapper'>
                     <h2>Hi There! I am</h2>
                     <h1>Saj Polwatta!</h1>
                     <h3>Software Devloper</h3>
