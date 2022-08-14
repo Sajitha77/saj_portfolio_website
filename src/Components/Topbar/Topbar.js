5452f648bfbcba8c4e226af60c7d0d6a7c6398b2
Saj Polwa <sajpolwa@gmail.com>
2022-08-14 00:53:57 -0400

    social platform
    -linkedin logo added
    -github logo added
    -contact logo added
    -links attached tched to logos

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -7,6 +7,19 @@ export default function Topbar({menuOpen, setMenuOpen}) {
                 <div className='left'>
                     <a href='#intro' className='logo'>Saj Polwatta</a>
                 </div>
+                <div className='middle'>
+                     <a href='"https://www.linkedin.com/in/sajpolwatta/'>
+                         <img src="assets/Linkedin-logo.svg" alt="LinkedIn logo/link to linkedin profile"></img>
+                         </a>
+                    <a href='"https://github.com/Sajitha77'>
+                        <img src="assets/github-logo.svg" alt="LinkedIn logo/link to linkedin profile"></img>
+                        </a>
+                    <a href='"#contact'>
+                        <img src="assets/Contact-logo.svg" alt="LinkedIn logo/link to linkedin profile"></img>
+                        </a>
+
+
+                </div>
                 <div className='right'>
                     <div className='navMenu' onClick={()=>setMenuOpen(!menuOpen)}>
                         <span className='line1'></span>
